8d06bfe935dbcc2770c5b0fba94e15e53c3c1af3
oliramilo <olimarramilo@yahoo.com>
2021-08-05 14:39:27 +0800

    created folder for commands

---
+++ commands/commands.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+/**Bot reply to a message by user**/
+func botReply(botid string, session *discordgo.Session, message *discordgo.MessageCreate) {
+	if botid != message.Author.ID {
+		//Reply
+	}
+
+}
+
+func processCommand(message string, commands []string) {
+	toks := strings.Split(message, " ")
+	command := strings.ToUpper(toks[0])
+	for _, sCommand := range commands {
+		if command == sCommand {
+			break
+		}
+	}
+}
